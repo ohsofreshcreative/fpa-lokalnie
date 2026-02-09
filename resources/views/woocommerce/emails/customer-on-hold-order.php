69f3e124936c173b25ee5052fdcf90f584470fa8
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-02-09 12:21:38 +0100

    Automatyczny deploy

--- resources/views/woocommerce/emails/customer-on-hold-order.php
+++ resources/views/woocommerce/emails/customer-on-hold-order.php
@@ -65,22 +65,7 @@ if ( ! empty( $items ) ) {
 }
 ?>
 
-<p>bardzo dziękujemy za rejestrację na konferencję <?php echo esc_html( $event_name ); ?>, która odbędzie się w dniu <?php echo esc_html( $event_date ); ?> w <?php echo esc_html( $event_place ); ?> w <?php echo esc_html( $event_city ); ?>. <b>Twoje zgłoszenie zostało przyjęte.</b></p>
-
-<b>Dane do przelewu:</b>
-<p>Evereth Publishing Sp. z o.o.<br>
-ul. Paprociowa 13<br>
-04-751 Warszawa<br><br>
-<b>nr konta:</b><br>
-63 1090 1753 0000 0001 4680 1378<br>
-<b>tytuł:</b><br> imię i nazwisko uczestnika – konferencja FZ2026.</p><br>
-
-<p>Wysokość opłat znajdziesz tutaj: <a href="https://forumzakazen.com/oplata-konferencyjna/">Opłata konferencyjna</a></p><br>
-
-<p>Prosimy o sprawdzenie, czy dane zawarte w formularzu są poprawne.</p><br>
-<p>W przypadku zauważonych błędów prosimy o dokonanie ponownej rejestracji na konferencję.</p><br>
-
-<p>W przypadku nieuiszczenia przez uczestnika opłaty w ciągu 7 dni Organizator jest zmuszony skreślić go z listy. Jeśli pomimo to uczestnik wyraża chęć wzięcia udziału w konferencji, konieczna jest ponowna rejestracja i dokonanie wpłaty (według cennika i obowiązującego terminarza znajdującego się w zakładce <a href="https://forumzakazen.com/oplata-konferencyjna/">Opłata konferencyjna</a>).</p>
+<p>bardzo dziękujemy za rejestrację na konferencję <?php echo esc_html( $event_name ); ?>, która odbędzie się w dniu <?php echo esc_html( $event_date ); ?> w <?php echo esc_html( $event_place ); ?>. <b>Twoje zgłoszenie oczekuje na płatność.</b></p>
 
 <?php
 
